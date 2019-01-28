a2948d0dc14b57834fb3cdf32e39fe52b95f4eef
luca1602 <blacksolution16@gmail.com>
2019-01-28 16:35:41 +0100

    added GUI(first version)

--- src/Employee/Menu.java
+++ src/Employee/Menu.java
@@ -1,13 +1,29 @@
 package Employee;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Menu {
 
 
     private JButton OKButton;
+    private JPanel panelMenu;
+
+    public Menu() {
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hauptmenü");
+        frame.setContentPane(new Menu().panelMenu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
